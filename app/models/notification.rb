e598da5b5eb3110f40917d992d419d6d47bf2640
Asaf Bartov <asaf.bartov@gmail.com>
2013-01-11 20:59:12 +0200

    longer snippet and different order in comment notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,9 +4,9 @@ class Notification < ActionMailer::Base
   helper :tasks
 
   def comment_added(comment, recipient_users)
-    subject     s_("comment added notification subject|New comment (%{domain}): %{task_name_snippet}") % {
-                  :task_name_snippet => comment.task.name.utf_snippet(20),
-                  :domain => domain}
+    subject     s_("comment added notification subject|%{task_name_snippet} - New comment") % {
+                  :task_name_snippet => comment.task.name.utf_snippet(40)}
+                # ,                  :domain => domain}
     from        from_address
     recipients  recipient_users.collect(&:email_recipient)
     sent_on     Time.now.utc
