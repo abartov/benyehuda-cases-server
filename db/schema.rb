d90028ca9d0dfb96eec9682e1401844bd2b12c33
Asaf Bartov <asaf.bartov@gmail.com>
2014-02-27 17:42:45 -0800

    switching to active record session store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131006191248) do
+ActiveRecord::Schema.define(:version => 20140228013947) do
 
   create_table "assignment_histories", :force => true do |t|
     t.integer  "user_id"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(:version => 20131006191248) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.boolean  "done"
   end
 
   create_table "global_preferences", :force => true do |t|
@@ -116,6 +117,16 @@ ActiveRecord::Schema.define(:version => 20131006191248) do
   end
 
   add_index "search_settings", ["user_id", "search_key"], :name => "index_search_settings_on_user_id_and_search_key"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "site_notices", :force => true do |t|
     t.datetime "start_displaying_at"
