f25a15262d7905c8e64030023a3cbbe222e5486e
Asaf Bartov <asaf.bartov@gmail.com>
2015-10-25 22:09:53 -0700

    priorities not required

--- app/models/task.rb
+++ app/models/task.rb
@@ -73,7 +73,7 @@ class Task < ActiveRecord::Base
   }
 
   validates :difficulty, :inclusion => {:in => DIFFICULTIES.keys, :message => "not included in the list"}
-  validates :priority, :inclusion => {:in => PRIORITIES.keys, :message => "not included in the list"}
+  #validates :priority, :inclusion => {:in => PRIORITIES.keys, :message => "not included in the list"}
   validates :creator_id, :name, :kind_id, :priority, :difficulty, :presence => true
   validate :parent_task_updated
 
