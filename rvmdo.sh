e83700a807bb477fb306ad9fd2c0c54969814ee4
Asaf Bartov <asaf.bartov@gmail.com>
2020-03-26 03:43:23 +0200

    Upped to Ruby 2.6

--- rvmdo.sh
+++ rvmdo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --login
 . ~/.profile
-rvm use 2.2
-echo 'rvmdo.sh using RAILS_ENV=production (!!) and Ruby 2.2'
-export RAILS_ENV=production 
+rvm use 2.6
+echo 'rvmdo.sh using RAILS_ENV=production (!!) and Ruby 2.6'
+export RAILS_ENV=production
 $1
