0d3645e2e09f749161419dc9589c6da8eeeb3628
Asaf Bartov <asaf.bartov@gmail.com>
2020-03-27 03:15:33 +0200

    Missed change from prev. commit

--- app/indices/user_index.rb
+++ app/indices/user_index.rb
@@ -1,6 +1,6 @@
 index_name = ENV['is_staging'] == 'true' ? 'staging_user' : 'user'
 
-ThinkingSphinx::Index.define :user, :with => :active_record do
+ThinkingSphinx::Index.define :user, name: index_name, :with => :active_record do
   indexes :name, :sortable => true
   indexes :email, :sortable => true
   indexes kind.name, :sortable => true, :as => :kind
