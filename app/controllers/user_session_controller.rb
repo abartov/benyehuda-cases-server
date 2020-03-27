696d017bd6be66817456b60cde61c308461db53f
Asaf Bartov <asaf.bartov@gmail.com>
2020-03-27 21:32:43 +0300

    Fixed login issue

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -4,22 +4,27 @@ class UserSessionController < InheritedResources::Base
   before_filter :require_user, :only => :destroy
   defaults :singleton => true
 
+  def do_create(object, &block)
+    respond_with_dual_blocks(object, {}, &block)
+  end
+
   def create
-    sess = UserSession.create(email: params[:user_session][:email], password: params[:user_session][:password], remember_me: params[:user_session][:remember_me])
-    if sess.valid?
-      sess.save
-      unless current_user.nil?
-        if current_user.disabled?
-          flash[:error] = _("Your account has been disabled, please contact support.")
-          flash.delete(:notice)
-          current_user_session.destroy
-          redirect_to login_path
+    @user_session = UserSession.create(email: params[:user_session][:email], password: params[:user_session][:password], remember_me: params[:user_session][:remember_me])
+    do_create(@user_session) do |success, failure|
+      success.html do
+        unless current_user.nil?
+          if current_user.disabled?
+            flash[:error] = _("Your account has been disabled, please contact support.")
+            flash.delete(:notice)
+            current_user_session.destroy
+            redirect_to login_path
+          else
+            redirect_to home_path
+          end
         else
+          current_user_session.destroy unless current_user_session.nil?
           redirect_to home_path
         end
-      else
-        current_user_session.destroy unless current_user_session.nil?
-        redirect_to home_path
       end
     end
   end
