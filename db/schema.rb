a6c26bfceeeb0aa02985172bd59f141a070aea8a
Asaf Bartov <asaf.bartov@gmail.com>
2020-07-21 16:55:31 +0300

    Added label for original language

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200720194645) do
+ActiveRecord::Schema.define(version: 20200721135352) do
 
   create_table "assignment_histories", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
