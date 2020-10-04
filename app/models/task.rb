34596c52260ecbaeadb4a7e7332179c693519061
Asaf Bartov <asaf.bartov@gmail.com>
2020-10-05 02:56:05 +0300

    Fixed escaping

--- app/models/task.rb
+++ app/models/task.rb
@@ -230,7 +230,7 @@ class Task < ActiveRecord::Base
     ret << self.parent unless self.parent.nil?
     slashpos = self.name.index('/')
     unless slashpos.nil? or slashpos < 5
-      possibly = Task.where('name like "?"', self.name[0..5]+'%'+self.name[slashpos..-1])
+      possibly = Task.where('name like ?', self.name[0..5]+'%'+self.name[slashpos..-1])
       possibly.each{|t| ret << t unless t.id == self.id}
     end
     return ret
