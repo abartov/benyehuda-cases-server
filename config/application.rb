0fb860ceabbc0016d51aa8bc9b7ae46acf681693
Asaf Bartov <asaf.bartov@gmail.com>
2020-10-04 22:10:11 +0300

    load lib/

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module CasesServer
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/app/middleware/)
 
+    config.autoload_paths << Rails.root.join('lib')
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
