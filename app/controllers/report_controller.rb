0a740125359aa6219d1c647dbcb4cc058df15eeb
Asaf Bartov <asaf.bartov@gmail.com>
2020-10-05 03:27:06 +0300

    Inverted task inclusion logic

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -34,7 +34,7 @@ class ReportController < InheritedResources::Base
 
   def missing_metadata
     @current_tab = :reports
-    @tasks = Task.where("state = 'ready_to_publish' AND (include_images is null or independent is null or genre is null)").paginate(:page => params[:page], :per_page => params[:per_page])
+    @tasks = Task.where("state <> 'ready_to_publish' AND (include_images is null or independent is null or genre is null)").paginate(:page => params[:page], :per_page => params[:per_page])
   end
   def missing_metadata_panel
     @task = Task.find(params[:id])
