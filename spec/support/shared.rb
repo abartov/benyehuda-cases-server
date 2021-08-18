4d723f2f2233e3ed00e537ad681d29792c1fd9f5
Asaf Bartov <asaf.bartov@gmail.com>
2021-08-18 17:49:21 +0300

    Removed debugging info

--- spec/support/shared.rb
+++ spec/support/shared.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples 'json result' do
   specify 'returns JSON' do
-    puts 'returns JSON'
+    #puts 'returns JSON'
     api_call params
     expect { JSON.parse(response.body) }.not_to raise_error
   end
@@ -8,14 +8,14 @@ end
 
 RSpec.shared_examples '400' do
   specify 'returns 400' do
-    puts 'returns 400'
+    #puts 'returns 400'
     api_call params
     expect(response.status).to eq(400)
   end
 end
 RSpec.shared_examples '401' do
   specify 'returns 401' do
-    puts 'returns 401'
+    #puts 'returns 401'
     api_call params
     expect(response.status).to eq(401)
   end
@@ -23,7 +23,7 @@ end
 
 RSpec.shared_examples 'contains error msg' do |msg|
   specify "error msg is #{msg}" do
-    puts "error msg is {msg}"
+    #puts "error msg is {msg}"
     api_call params
     json = JSON.parse(response.body)
     expect(json['error'] || json['error_msg']).to eq(msg)
@@ -32,7 +32,7 @@ end
 
 RSpec.shared_examples '200' do
   specify 'returns 200' do
-    puts "returns 200"
+    #puts "returns 200"
     api_call params
     expect(response.status).to eq(200)
   end
@@ -65,7 +65,7 @@ end
 
 RSpec.shared_examples 'contains error code' do |code|
   specify "error code is #{code}" do
-    puts "error code is {code}"
+    #puts "error code is {code}"
     api_call params
     json = JSON.parse(response.body)
     expect(json['error_code']).to eq(code)
@@ -74,7 +74,7 @@ end
 
 RSpec.shared_examples 'contains error msg with devheader' do |msg|
   specify "error msg is #{msg}" do
-    puts "error msg is {msg}"
+    #puts "error msg is {msg}"
     api_call params
     json = JSON.parse(response.body)
     expect(json['error_msg']).to eq(msg)
@@ -83,7 +83,7 @@ end
 
 RSpec.shared_examples 'auditable created' do
   specify 'creates an api call audit' do
-    puts "creates an api call audit"
+    #puts "creates an api call audit"
     expect do
       api_call params
     end.to change{ AuditLog.count }.by(1)
