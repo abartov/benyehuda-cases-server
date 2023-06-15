c5e9ac77598eb04812c0371fbfc2c1124ad7a2cf
Asaf Bartov <asaf.bartov@gmail.com>
2023-06-15 08:00:59 -0700

    HAML upgrade

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,26 +13,26 @@ module ApplicationHelper
 
   def render_tabs
     return unless logged_in?
-    content_tag(:ul, :id => "nav") do #  :class => "tabs") do
-      TabsHelper::TABS.reverse.each do |tab|
-        next if tab[:if] && !current_user.send(tab[:if])
+    buf = "<ul id=\"nav\">"
+    TabsHelper::TABS.reverse.each do |tab|
+      next if tab[:if] && !current_user.send(tab[:if])
 
-        opts = {}
-        opts[:class] = "active" if @current_tab == tab[:name]
+      opts = {}
+      opts[:class] = "active" if @current_tab == tab[:name]
 
-        content_tag(:li, opts) do
-          content_tag(:a, :href => tab[:path]) do
-            content_tag :span, s_(tab[:title])
-          end
-        end
-      end
+      buf += "<li " + (@current_tab == tab[:name] ? 'class="active"' : '') +'>'
+      buf += "<a href=\"#{tab[:path]}\"><span>#{s_(tab[:title])}</span></a></li>"
     end
+    buf += "</ul>"
+    buf
   end
 
   def site_notices
+    buf = ''
     SiteNotice.active.each do |sn|
-      content_tag(:div, sn.html.html_safe, :class => "site-notice")
+      buf += "<div class='site-notice'>#{sn.html.html_safe}</div>"
     end
+    buf
   end
 
   def boolean_property(value)
@@ -40,6 +40,7 @@ module ApplicationHelper
   end
   # replacement for Rails 3.x function
   def link_to_function(name, function, html_options={})
+    # return "<a href='javascript:void(0);' onclick='#{function}; return false;'>#{name}</a>"
     content_tag(:a, name, {:href => 'javascript:void(0);', :onclick => "#{function}; return false;"}.merge(html_options))
   end
   def password_edit_title
