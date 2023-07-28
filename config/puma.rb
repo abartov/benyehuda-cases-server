49436d4b79c85f0bd6761e014ad829818bcb5510
Asaf Bartov <asaf.bartov@gmail.com>
2023-07-28 16:40:03 +0300

    no shared dir

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,16 @@
 app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}"
 rackup(File.expand_path('../config.ru', __dir__))
 if ENV['RACK_ENV'] == 'production'
   require 'puma/daemon'
   environment 'production'
   workers Integer(ENV['WEB_CONCURRENCY'] || 3)
   daemonize
-  shared_dir = "#{app_dir}/shared"
   bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 else
   workers 0
   environment 'development'
-  shared_dir = "#{app_dir}"
 end
 
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
