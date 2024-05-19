1cc594229f85b45a12a0a789cd761904c6a610de
Asaf Bartov <asaf.bartov@gmail.com>
2024-05-19 20:02:26 +0300

    Added notification on task publishing

--- app/models/task_notifications.rb
+++ app/models/task_notifications.rb
@@ -1,6 +1,6 @@
 module TaskNotifications
 
-  SKIP_STATES = [:unassigned, :ready_to_publish, :other_task_creat]
+  SKIP_STATES = [:unassigned, :other_task_creat]
 
   def self.included(base)
     base.class_eval do
@@ -19,7 +19,8 @@ module TaskNotifications
     state_changed_by = []
     state_changed_for = []
     if @was_state_changed || comment
-      state_changed_for = [editor, assignee]
+      state_changed_for << assignee
+      state_changed_for << editor if comment.present? || self.state != 'ready_to_publish'
       state_changed_by << comment.user if comment.try(:user)
     elsif @was_editor_id_changed || @was_assignee_id_changed
       state_changed_for << editor if @was_editor_id_changed
@@ -33,10 +34,18 @@ module TaskNotifications
 
   # XXX
   def delayed_notify_on_changes
+    debugger
     return if SKIP_STATES.member?(state.to_sym)
     recipients = (task_changes_recipients || []).select {|r| r.wants_to_be_notified_of?(:state)}
     return if recipients.blank?
 
-    I18n.with_locale('he') { Notification.task_state_changed(self, recipients).deliver }
+    cond = @was_state_changed
+    cond = false unless state == 'ready_to_publish'
+    if cond
+    #if @was_stated_changed && self.state == 'ready_to_publish'
+      I18n.with_locale('he') { Notification.task_published(self, recipients).deliver }
+    else
+      I18n.with_locale('he') { Notification.task_state_changed(self, recipients).deliver }
+    end
   end
 end
