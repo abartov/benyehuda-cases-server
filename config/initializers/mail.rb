f6f5361f5ec9f59c783337e2253d5cd5ed8b8ea4
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-20 14:28:24 +0500

    Updated mailer settings to use Gmail's smtp and moved credentials to ENV variables

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,18 @@
-domain = (GlobalPreference.get(:domain) || "benyehuda.org") rescue "benyehuda.org"
-ActionMailer::Base.smtp_settings = {
-  :address => "localhost",
-  :port => 25,
-  :domain => domain,
-  :enable_starttls_auto => false  # XXX maybe to set through global preferences
-}
+# frozen_string_literal: true
+
+require 'deployment_helpers'
+
+if Rails.env.production? && !DeploymentHelpers.assets_compilation?
+  # Settings below assumes we use Gmail's SMTP for sending emails
+  # For this you'll need to get Gmail Application Password as described here:
+  # https://support.google.com/mail/answer/185833?hl=en
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV.fetch('SMTP_USERNAME'),
+    password: ENV.fetch('SMTP_PASSWORD'),
+    address: ENV.fetch('SMTP_HOST'),
+    port: ENV.fetch('SMTP_PORT'),
+    domain: ENV.fetch('SMTP_DOMAIN'),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
