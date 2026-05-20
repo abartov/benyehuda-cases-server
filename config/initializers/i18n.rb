b074ee150d66b8ff51b7f062ef1cdaba9421c5e0
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-20 14:28:24 +0500

    Updated mailer settings to use Gmail's smtp and moved credentials to ENV variables

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "fast_gettext/translation_repository/db"
 FastGettext::TranslationRepository::Db.require_models
 
@@ -12,22 +14,10 @@ I18n.enforce_available_locales = false
 
 
 begin
-  def assets_compilation?
-    # Check if we're in assets group (set by rails assets:precompile)
-    return true if ENV['RAILS_GROUPS'].to_s.include?('assets')
-
-    # Fallback to checking rake tasks
-    if defined?(Rake.application)
-      Rake.application.top_level_tasks.any? do |task|
-        task.to_s == 'assets:precompile' ||
-          task.to_s.start_with?('assets:precompile')
-      end
-    end
-  end
-
+  require 'deployment_helpers'
   # The code below requires DB connection, which can be unavailable during assets precompilation
   # so we do it only when it is not an assets compilation rake task
-  unless assets_compilation?
+  unless DeploymentHelpers.assets_compilation?
     WillPaginate::ViewHelpers.pagination_options[:previous_label] = s_('paginator - previous page|&laquo; Previous')
     WillPaginate::ViewHelpers.pagination_options[:next_label] = s_('paginator - previous page|Next &raquo;')
   end
