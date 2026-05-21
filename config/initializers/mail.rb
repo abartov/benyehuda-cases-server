d5196ca8ff156ba0cba162455f6556014f18e78e
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-05-21 13:42:12 +0500

    switched to use Gmail's smtp-relay service

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
-require 'deployment_helpers'
-
-if Rails.env.production? && !DeploymentHelpers.assets_compilation?
-  # Settings below assumes we use Gmail's SMTP for sending emails
-  # For this you'll need to get Gmail Application Password as described here:
-  # https://support.google.com/mail/answer/185833?hl=en
+if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV.fetch('SMTP_USERNAME'),
-    password: ENV.fetch('SMTP_PASSWORD'),
-    address: ENV.fetch('SMTP_HOST'),
-    port: ENV.fetch('SMTP_PORT'),
-    domain: ENV.fetch('SMTP_DOMAIN'),
-    authentication: 'plain',
-    enable_starttls_auto: true
+    address: 'smtp-relay.gmail.com',
+    domain: 'benyehuda.org'
   }
 end
