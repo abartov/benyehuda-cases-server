f21eccb8336cf917179972f9e8c4174911968434
Asaf Bartov <asaf.bartov@gmail.com>
2026-06-12 08:18:11 +0300

    Fix Ruby 3.2 and ActiveSupport 6.1 gotcha

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'logger' # ActiveSupport 6.1 assumes Logger is loaded; explicit in Ruby 3.2+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
