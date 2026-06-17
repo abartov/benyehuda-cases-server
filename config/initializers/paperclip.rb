46522a6eda64101a82ce3f028102a5eea602750f
Damir Sultanbekov <Damir.Sultanbekov@gmail.com>
2026-06-18 01:30:36 +0500

    updated paperclip initializer to use SiteConstants instead of fetching settings from ENV vars

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,18 +1,14 @@
-require 'deployment_helpers'
-
-unless DeploymentHelpers.assets_compilation?
-  Paperclip.options[:content_type_mappings] = {
-    jpg: %w[image/jpeg],
-    jpeg: %w[image/jpeg]
-  }
-  Paperclip::Attachment.default_options[:storage] = :s3
-  Paperclip::Attachment.default_options[:s3_protocol] = :https
-  Paperclip::Attachment.default_options[:s3_region] = ENV.fetch('S3_REGION')
-  Paperclip::Attachment.default_options[:bucket] = ENV.fetch('S3_BUCKET')
-  Paperclip::Attachment.default_options[:s3_credentials] = {
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  }
-  # Paperclip::DataUriAdapter.register
-  Paperclip::HttpUrlProxyAdapter.register
-end
+Paperclip.options[:content_type_mappings] = {
+  jpg: %w[image/jpeg],
+  jpeg: %w[image/jpeg]
+}
+Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:s3_protocol] = :https
+Paperclip::Attachment.default_options[:s3_region] = SiteConstants::S3_REGION
+Paperclip::Attachment.default_options[:bucket] = SiteConstants::S3_BUCKET
+Paperclip::Attachment.default_options[:s3_credentials] = {
+  access_key_id: SiteConstants::AWS_ACCESS_KEY_ID,
+  secret_access_key: SiteConstants::AWS_SECRET_ACCESS_KEY
+}
+# Paperclip::DataUriAdapter.register
+Paperclip::HttpUrlProxyAdapter.register
